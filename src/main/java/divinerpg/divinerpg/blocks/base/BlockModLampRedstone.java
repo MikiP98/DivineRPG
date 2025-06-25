8b1df5c245e2f0a8bc2d84bc1982ce65a7736767
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-25 16:35:59 +0200

    Ported BlockModLampRedstone to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModLampRedstone.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModLampRedstone.java
@@ -1,12 +1,10 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.world.level.material.MapColor;
-
-import static net.minecraft.world.level.block.Blocks.REDSTONE_LAMP;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
 
 public class BlockModLampRedstone extends BlockModPowered {
     public BlockModLampRedstone() {
-        super(Properties.ofFullCopy(REDSTONE_LAMP).mapColor(MapColor.COLOR_GREEN)
-                .lightLevel((state) -> state.getValue(POWERED) ? 15 : 0));
+        super(Settings.copy(Blocks.REDSTONE_LAMP).mapColor(MapColor.GREEN).luminance((state) -> state.get(POWERED) ? 15 : 0));
     }
 }
