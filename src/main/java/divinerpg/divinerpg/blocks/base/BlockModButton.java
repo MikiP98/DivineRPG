464e0082c7169914ffd9e11d00f3b42fa02df80b
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-25 10:21:05 +0200

    Ported BlockModButton to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModButton.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModButton.java
@@ -1,11 +1,12 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.properties.BlockSetType;
-
-import static net.minecraft.world.level.block.state.properties.BlockSetType.STONE;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockSetType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ButtonBlock;
 
 public class BlockModButton extends ButtonBlock {
-    public BlockModButton(BlockSetType type) {super(type, 30, Properties.ofFullCopy(Blocks.OAK_BUTTON));}
-    public BlockModButton() {super(STONE, 20, Properties.ofFullCopy(Blocks.STONE_BUTTON));}
+    // TODO: Make sure wooden=true is correct
+    public BlockModButton(BlockSetType type) {super(Block.Settings.copy(Blocks.OAK_BUTTON), type, 30, true);}
+    public BlockModButton() {super(Block.Settings.copy(Blocks.STONE_BUTTON), BlockSetType.STONE, 20, false);}
 }
