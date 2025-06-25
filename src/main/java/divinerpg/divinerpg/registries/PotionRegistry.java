47fc7f61205d869fb85b7d4694de838e945f122b
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-25 17:28:21 +0200

    Ported PotionRegistry to Fabric

--- src/main/java/divinerpg/divinerpg/registries/PotionRegistry.java
+++ src/main/java/divinerpg/divinerpg/registries/PotionRegistry.java
@@ -1,18 +1,21 @@
-package divinerpg.registries;
+package divinerpg.divinerpg.registries;
 
-import divinerpg.DivineRPG;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.item.alchemy.Potion;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
+import static divinerpg.divinerpg.DivineRPG.getId;
 
 public class PotionRegistry {
-    public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(BuiltInRegistries.POTION, DivineRPG.MODID);
-    public static final DeferredHolder<Potion, Potion>
-    	TEA = POTIONS.register("tea", () -> new Potion(new MobEffectInstance(MobEffectRegistry.FREEZE_REDUCTION, 1))),
-    	WARMTH = POTIONS.register("warmth", () -> new Potion(new MobEffectInstance(MobEffectRegistry.WARMTH, 1800))),
-    	LONG_WARMTH = POTIONS.register("long_warmth", () -> new Potion(new MobEffectInstance(MobEffectRegistry.WARMTH, 2500))),
-    	GROG = POTIONS.register("grog", () -> new Potion(new MobEffectInstance(MobEffects.CONFUSION, 250)));
+    public static final Potion
+			TEA = register("tea", new Potion(new StatusEffectInstance(MobEffectRegistry.FREEZE_REDUCTION, 1))),
+			WARMTH = register("warmth", new Potion(new StatusEffectInstance(MobEffectRegistry.WARMTH, 1800))),
+			LONG_WARMTH = register("long_warmth", new Potion(new StatusEffectInstance(MobEffectRegistry.WARMTH, 2500))),
+			GROG = register("grog", new Potion(new StatusEffectInstance(StatusEffects.NAUSEA, 250)));
+
+	public static Potion register(String name, Potion potion) {
+		return Registry.register(Registries.POTION, getId(name), potion);
+	}
 }
