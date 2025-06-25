bf1b690c833399c88378772b7feda09e5c8532e2
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-25 20:37:40 +0200

    Ported MarkerEffect to Fabric

--- src/main/java/divinerpg/divinerpg/effect/mob/armor/base/MarkerEffect.java
+++ src/main/java/divinerpg/divinerpg/effect/mob/armor/base/MarkerEffect.java
@@ -1,7 +1,7 @@
-package divinerpg.effect.mob.armor.base;
+package divinerpg.divinerpg.effect.mob.armor.base;
 
-import divinerpg.effect.mob.armor.ArmorEffect;
+import divinerpg.divinerpg.effect.mob.armor.ArmorEffect;
 
 public class MarkerEffect extends ArmorEffect {
-	public MarkerEffect() {super(10991286);}
+	public MarkerEffect() { super(10991286); }
 }
