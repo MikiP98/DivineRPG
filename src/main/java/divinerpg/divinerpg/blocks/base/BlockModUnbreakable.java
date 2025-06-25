4495fd4dad6456e4978b0bc29622c62299acf3a1
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-25 14:29:19 +0200

    Ported BlockModUnbreakable to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModUnbreakable.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModUnbreakable.java
@@ -1,14 +1,16 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.world.level.material.MapColor;
-
-import static net.minecraft.world.level.block.Blocks.SEA_LANTERN;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
 
 public class BlockModUnbreakable extends BlockMod {
     //Unbreakable stone-like blocks
-    public BlockModUnbreakable(MapColor color) {super(color, -1, 3600000);}
+    public BlockModUnbreakable(MapColor color) { super(color, -1, 3600000); }
     //Unbreakable stone-like blocks with custom properties
-    public BlockModUnbreakable(Properties properties) {super(properties.strength(-1, 3600000));}
+    public BlockModUnbreakable(Settings settings) { super(settings.strength(-1, 3600000)); }
     //Unbreakable lantern-like blocks
-    public BlockModUnbreakable(MapColor color, int lightLevel) {this(Properties.ofFullCopy(SEA_LANTERN).mapColor(color).lightLevel((state) -> lightLevel));}
+    public BlockModUnbreakable(MapColor color, int lightLevel) {
+        this(Block.Settings.copy(Blocks.SEA_LANTERN).mapColor(color).luminance((state) -> lightLevel));
+    }
 }
