33a4b29af7974687e484e069c7c54c1d3fa99c2a
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-25 10:44:13 +0200

    Ported BlockModCrop to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModCrop.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModCrop.java
@@ -1,29 +1,34 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.*;
-
-import static net.minecraft.world.level.block.Blocks.WHEAT;
+import net.minecraft.block.*;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 
 public class BlockModCrop extends CropBlock {
-    private final ItemLike seed;
+    private final ItemConvertible seed;
     private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
-            box(0, 0, 0, 16, 3, 16),
-            box(0, 0, 0, 16, 6, 16),
-            box(0, 0, 0, 16, 9, 16),
-            box(0, 0, 0, 16, 11, 16),
-            box(0, 0, 0, 16, 11, 16),
-            box(0, 0, 0, 16, 11, 16),
-            box(0, 0, 0, 16, 11, 16),
-            box(0, 0, 0, 16, 11, 16)
+            createCuboidShape(0, 0, 0, 16, 3, 16),
+            createCuboidShape(0, 0, 0, 16, 6, 16),
+            createCuboidShape(0, 0, 0, 16, 9, 16),
+            createCuboidShape(0, 0, 0, 16, 11, 16),
+            createCuboidShape(0, 0, 0, 16, 11, 16),
+            createCuboidShape(0, 0, 0, 16, 11, 16),
+            createCuboidShape(0, 0, 0, 16, 11, 16),
+            createCuboidShape(0, 0, 0, 16, 11, 16)
     };
-    public BlockModCrop(ItemLike seed) {
-        super(Properties.ofFullCopy(WHEAT));
+
+    public BlockModCrop(ItemConvertible seed) {
+        super(Block.Settings.copy(Blocks.WHEAT));
         this.seed = seed;
     }
-    @Override public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {return SHAPE_BY_AGE[state.getValue(getAgeProperty())];}
-    @Override protected ItemLike getBaseSeedId() {return seed;}
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return SHAPE_BY_AGE[state.get(getAgeProperty())];
+    }
+
+    @Override
+    protected ItemConvertible getSeedsItem() { return seed; }
 }
