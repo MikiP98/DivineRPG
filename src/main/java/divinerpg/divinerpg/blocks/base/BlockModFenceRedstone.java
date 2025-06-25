1ace30a61ec135b8a0ae78e1e107979226357e7d
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-25 13:30:34 +0200

    Ported BlockModFenceRedstone to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModFenceRedstone.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModFenceRedstone.java
@@ -1,57 +1,72 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.protocol.game.DebugPackets;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.*;
-import net.minecraft.world.level.block.state.properties.*;
-import net.minecraft.world.level.material.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FenceBlock;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
 
 public class BlockModFenceRedstone extends FenceBlock {
-    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+    public static final BooleanProperty POWERED = Properties.POWERED;
+
     public BlockModFenceRedstone(MapColor color) {
-        super(Block.Properties.of().mapColor(color).dynamicShape().strength(0.3F).sound(SoundType.GLASS).instrument(NoteBlockInstrument.HAT));
-        this.registerDefaultState(this.stateDefinition.any().setValue(POWERED, Boolean.valueOf(false)).setValue(NORTH, Boolean.valueOf(false)).setValue(EAST, Boolean.valueOf(false)).setValue(SOUTH, Boolean.valueOf(false)).setValue(WEST, Boolean.valueOf(false)).setValue(WATERLOGGED, Boolean.valueOf(false)));
-
+        super(Block.Settings.create()
+                .mapColor(color)
+                .dynamicBounds()
+                .strength(0.3F)
+                .sounds(BlockSoundGroup.GLASS)
+                .instrument(Instrument.HAT)
+                .luminance(state -> state.get(POWERED) ? 15 : 0)
+        );
+        setDefaultState(getDefaultState()
+                .with(POWERED, Boolean.FALSE)
+                .with(NORTH, Boolean.FALSE)
+                .with(EAST, Boolean.FALSE)
+                .with(SOUTH, Boolean.FALSE)
+                .with(WEST, Boolean.FALSE)
+                .with(WATERLOGGED, Boolean.FALSE)
+        );
     }
 
+    @SuppressWarnings("deprecation")
     @Override
-    public int getLightEmission(BlockState state, BlockGetter level, BlockPos pos) {
-        return state.getValue(POWERED) ? 15 : 0;
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        handleBlockState(state, world, pos);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
-    public void onPlace(BlockState state, Level worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
-        handleBlockState(state, worldIn, pos);
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+        handleBlockState(state, world, pos);
+        super.neighborUpdate(state, world, pos, sourceBlock, sourcePos, notify);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
-    public void neighborChanged(BlockState state, Level worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
-        handleBlockState(state, worldIn, pos);
-        DebugPackets.sendNeighborsUpdatePacket(worldIn, pos);
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        handleBlockState(state, world, pos);
     }
 
-    @Override
-    public void tick(BlockState state, ServerLevel worldIn, BlockPos pos, RandomSource rand) {
-        handleBlockState(state, worldIn, pos);
-    }
-
-
-
-    public void handleBlockState(BlockState state, Level worldIn, BlockPos pos) {
-        if (!worldIn.isClientSide) {
-            if (state.getValue(POWERED) && !worldIn.hasNeighborSignal(pos)) {
-                worldIn.setBlock(pos, state.setValue(POWERED, false), 2);
-            } else if (!state.getValue(POWERED) && worldIn.hasNeighborSignal(pos)) {
-                worldIn.setBlock(pos, state.setValue(POWERED, Boolean.valueOf(true)), 2);
+    public void handleBlockState(BlockState state, World world, BlockPos pos) {
+        if (!world.isClient) {
+            if (state.get(POWERED) && !world.isReceivingRedstonePower(pos)) {
+                // TODO: Make sure 2 is the correct flag for updating the block state, as vanilla uses 4 (I think)
+                world.setBlockState(pos, state.with(POWERED, false), 2);
+            } else if (!state.get(POWERED) && world.isReceivingRedstonePower(pos)) {
+                world.setBlockState(pos, state.with(POWERED, Boolean.TRUE), 2);
             }
         }
     }
 
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(NORTH, EAST, WEST, SOUTH, WATERLOGGED, POWERED);
     }
 }
