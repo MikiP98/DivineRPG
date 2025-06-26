be8949299ebabbb07b99eca850de2346a11cd229
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 15:40:28 +0200

    Ported HeavyAirEffect to Fabric

--- src/main/java/divinerpg/divinerpg/effect/mob/HeavyAirEffect.java
+++ src/main/java/divinerpg/divinerpg/effect/mob/HeavyAirEffect.java
@@ -1,31 +1,45 @@
-package divinerpg.effect.mob;
+package divinerpg.divinerpg.effect.mob;
 
-import divinerpg.registries.BlockRegistry;
-import divinerpg.registries.MobEffectRegistry;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.effect.*;
-import net.minecraft.world.entity.*;
-import net.minecraft.world.level.GameType;
+import divinerpg.divinerpg.registries.BlockRegistry;
+import divinerpg.divinerpg.registries.MobEffectRegistry;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameMode;
 
-public class HeavyAirEffect extends MobEffect {
-	public HeavyAirEffect() {super(MobEffectCategory.HARMFUL, 10991286);}
-	@Override public void onEffectAdded(LivingEntity entity, int amplifier) {
-		super.onEffectAdded(entity, amplifier);
-		if(entity instanceof ServerPlayer s && s.gameMode.getGameModeForPlayer() == GameType.SURVIVAL) s.setGameMode(GameType.ADVENTURE);
-	}
-	@Override public void onMobRemoved(LivingEntity entity, int amplifier, Entity.RemovalReason reason) {
-		if(entity instanceof ServerPlayer s && s.gameMode.getGameModeForPlayer() == GameType.ADVENTURE) s.setGameMode(GameType.SURVIVAL);
-	}
+public class HeavyAirEffect extends StatusEffect {
+	public HeavyAirEffect() { super(StatusEffectCategory.HARMFUL, 10991286); }
+
 	@Override
-	public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
-		return duration == 10;
+	public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+		super.onApplied(entity, attributes, amplifier);
+		if (entity instanceof ServerPlayerEntity serverPlayerEntity && serverPlayerEntity.interactionManager.getGameMode() == GameMode.SURVIVAL)
+			serverPlayerEntity.changeGameMode(GameMode.ADVENTURE);
 	}
-	@Override public boolean applyEffectTick(LivingEntity entity, int i) {
-		if(entity.level().getBlockState(new BlockPos(entity.getBlockX(), (int) (entity.getY() + entity.getEyeHeight()), entity.getBlockZ())).is(BlockRegistry.dungeonAir)) {
-			entity.addEffect(new MobEffectInstance(MobEffectRegistry.HEAVY_AIR, 20, 1, true, false, false));
-			return true;
-		} if(entity instanceof ServerPlayer s && s.gameMode.getGameModeForPlayer() == GameType.ADVENTURE) s.setGameMode(GameType.SURVIVAL);
-		return false;
+
+	@Override
+	public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+		if (entity instanceof ServerPlayerEntity serverPlayerEntity && serverPlayerEntity.interactionManager.getGameMode() == GameMode.ADVENTURE)
+			serverPlayerEntity.changeGameMode(GameMode.DEFAULT);
+			// TODO: Make sure changing SURVIVAL to DEFAULT works as intended
+	}
+
+	@Override
+	public boolean canApplyUpdateEffect(int duration, int amplifier) {
+		return duration == 10;
+		// TODO: Make sure 'duration % 10 == 0' isn't better than current implementation from NeoForge DivineRPG
+	}
+
+	@Override
+	public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+		if(entity.getWorld().getBlockState(new BlockPos(entity.getBlockX(), (int) (entity.getY() + entity.getEyeHeight(entity.getPose())), entity.getBlockZ())).isOf(BlockRegistry.dungeonAir)) {
+			entity.addStatusEffect(new StatusEffectInstance(MobEffectRegistry.HEAVY_AIR, 20, 1, true, false, false));
+		} if(entity instanceof ServerPlayerEntity serverPlayerEntity && serverPlayerEntity.interactionManager.getGameMode() == GameMode.ADVENTURE)
+			serverPlayerEntity.changeGameMode(GameMode.DEFAULT);
+			// TODO: Make sure changing SURVIVAL to DEFAULT works as intended
 	}
 }
