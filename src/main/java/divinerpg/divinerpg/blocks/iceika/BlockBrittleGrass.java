4fa54f901b840363c9694a48492391fe29d53344
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 23:28:52 +0200

    Ported BlockBrittleGrass to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/iceika/BlockBrittleGrass.java
+++ src/main/java/divinerpg/divinerpg/blocks/iceika/BlockBrittleGrass.java
@@ -1,56 +1,79 @@
-package divinerpg.blocks.iceika;
+package divinerpg.divinerpg.blocks.iceika;
 
-import com.mojang.serialization.MapCodec;
-import net.minecraft.core.*;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.*;
-import net.minecraft.world.level.block.state.properties.*;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraft.world.phys.shapes.*;
+import divinerpg.divinerpg.blocks.AlwaysFlammable;
+import net.minecraft.block.*;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
-public class BlockBrittleGrass extends BushBlock {
-	public static final MapCodec<BlockBrittleGrass> CODEC = simpleCodec(BlockBrittleGrass::new);
-	public static final BooleanProperty HANGING = BlockStateProperties.HANGING;
-	protected static final VoxelShape SHAPE = Block.box(2, 0, 2, 14, 12, 14), HANGING_SHAPE = Block.box(2, 4, 2, 14, 16, 14);
-	@Override public MapCodec<BlockBrittleGrass> codec() {return CODEC;}
-	public BlockBrittleGrass(Properties properties) {
-		super(properties.mapColor(MapColor.GLOW_LICHEN).sound(SoundType.MOSS_CARPET).dynamicShape().offsetType(BlockBehaviour.OffsetType.XZ));
-		registerDefaultState(this.stateDefinition.any().setValue(HANGING, false));
+public class BlockBrittleGrass extends PlantBlock implements AlwaysFlammable {
+	public static final BooleanProperty HANGING = Properties.HANGING;
+	protected static final VoxelShape
+			SHAPE = Block.createCuboidShape(2, 0, 2, 14, 12, 14),
+			HANGING_SHAPE = Block.createCuboidShape(2, 4, 2, 14, 16, 14);
+	
+	public BlockBrittleGrass(Settings settings) {
+		super(settings.mapColor(MapColor.LICHEN_GREEN).sounds(BlockSoundGroup.MOSS_CARPET).dynamicBounds().offset(AbstractBlock.OffsetType.XZ));
+		setDefaultState(getDefaultState().with(HANGING, false));
 	}
+	
 	@Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(HANGING);
-    }
-	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter p_154526_, BlockPos p_154527_, CollisionContext p_154528_) {
-		return state.getValue(HANGING) ? HANGING_SHAPE : SHAPE;
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(HANGING);
 	}
+	
+	@SuppressWarnings("deprecation")
 	@Override
-	protected boolean mayPlaceOn(BlockState state, BlockGetter getter, BlockPos pos) {
-		return getter.getBlockState(pos.below()).isFaceSturdy(getter, pos.below(), Direction.UP) || getter.getBlockState(pos.above()).isFaceSturdy(getter, pos.above(), Direction.DOWN);
+	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+		return state.get(HANGING) ? HANGING_SHAPE : SHAPE;
 	}
+	
 	@Override
-	public BlockState updateShape(BlockState state, Direction direction, BlockState s, LevelAccessor level, BlockPos pos, BlockPos p) {
-		return level.getBlockState(pos.above()).isFaceSturdy(level, pos.above(), Direction.DOWN) ? defaultBlockState().setValue(HANGING, true) : level.getBlockState(pos.below()).isFaceSturdy(level, pos.below(), Direction.UP) ? defaultBlockState() : Blocks.AIR.defaultBlockState();
+	protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+		return world.getBlockState(pos.down()).isSideSolidFullSquare(world, pos.down(), Direction.UP)
+				|| world.getBlockState(pos.up()).isSideSolidFullSquare(world, pos.up(), Direction.DOWN);
 	}
+	
 	@Override
-	public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
-		return mayPlaceOn(null, level, pos);
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+		return world.getBlockState(pos.up()).isSideSolidFullSquare(world, pos.up(), Direction.DOWN)
+				? getDefaultState().with(HANGING, true)
+				: world.getBlockState(pos.down()).isSideSolidFullSquare(world, pos.down(), Direction.UP)
+						? getDefaultState()
+						: Blocks.AIR.getDefaultState();
 	}
+	
 	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		return context.getLevel().getBlockState(context.getClickedPos().above()).isFaceSturdy(context.getLevel(), context.getClickedPos().above(), Direction.DOWN) ? defaultBlockState().setValue(HANGING, true) : context.getLevel().getBlockState(context.getClickedPos().below()).isFaceSturdy(context.getLevel(), context.getClickedPos().below(), Direction.UP) ? defaultBlockState() : Blocks.AIR.defaultBlockState();
+	public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+		return canPlantOnTop(null, world, pos);
+	}
+	
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		World world = ctx.getWorld();
+		BlockPos pos = ctx.getBlockPos();
+		return world.getBlockState(pos.up()).isSideSolidFullSquare(world, pos.up(), Direction.DOWN)
+				? getDefaultState().with(HANGING, true)
+				: world.getBlockState(pos.down()).isSideSolidFullSquare(world, pos.down(), Direction.UP)
+						? getDefaultState()
+						: Blocks.AIR.getDefaultState();
 	}
 
-        @Override
-        public int getFlammability(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {
-            return 100;
-        }
-
-        @Override
-        public int getFireSpreadSpeed(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {
-            return 60;
-    }
+	@Override
+	public int getFlammability() {
+		return 100;
+	}
+	@Override
+	public int getFireSpreadSpeed() {
+		return 60;
+	}
 }
