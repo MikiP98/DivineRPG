73bc5f315ea9b659749ae777f46b8fcd3fe88c76
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 18:23:07 +0200

    Ported BlockModPressurePlate to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModPressurePlate.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModPressurePlate.java
@@ -1,14 +1,13 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.properties.BlockSetType;
-import net.minecraft.world.level.material.MapColor;
+import net.minecraft.block.*;
 
 public class BlockModPressurePlate extends PressurePlateBlock {
     public BlockModPressurePlate(MapColor color, BlockSetType type) {
-        super(type, Properties.ofFullCopy(Blocks.OAK_PRESSURE_PLATE).mapColor(color));
+        super(ActivationRule.EVERYTHING, Settings.copy(Blocks.OAK_PRESSURE_PLATE).mapColor(color), type);
     }
     public BlockModPressurePlate(Block copy, MapColor color, BlockSetType type) {
-        super(type, Properties.ofFullCopy(copy).mapColor(color).requiresCorrectToolForDrops());
+        super(ActivationRule.EVERYTHING, Settings.copy(copy).mapColor(color).requiresTool(), type);
     }
+    // TODO: Make sure 'ActivationRule.EVERYTHING' is the correct rule for this block.
 }
