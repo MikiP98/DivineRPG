e6c1d583ca1d7a6f1d9ee101d1d42aa4f0695e40
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 18:26:32 +0200

    Ported BlockModSlab to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModSlab.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModSlab.java
@@ -1,19 +1,22 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.core.*;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockState;
+import divinerpg.divinerpg.blocks.AlwaysFlammable;
+import net.minecraft.block.Block;
+import net.minecraft.block.SlabBlock;
 
-public class BlockModSlab extends SlabBlock {
+public class BlockModSlab extends SlabBlock implements AlwaysFlammable {
     private int flammability, fireSpread;
+
     public BlockModSlab(Block base) {
-        super(Properties.ofFullCopy(base));
-        if(base instanceof BlockModPlanks) {
+        super(Settings.copy(base));
+        if (base instanceof BlockModPlanks) {
             flammability = 20;
             fireSpread = 5;
         }
     }
-    @Override public int getFlammability(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {return flammability;}
-    @Override public int getFireSpreadSpeed(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {return fireSpread;}
+
+    @Override
+    public int getFlammability() { return flammability; }
+    @Override
+    public int getFireSpreadSpeed() { return fireSpread; }
 }
