ea3b80ec41d8871f0df3604fba18c2547e4e7e6f
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 11:35:38 +0200

    Ported BlockModMobCage to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModMobCage.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModMobCage.java
@@ -1,60 +1,86 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.*;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.*;
-import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
 import javax.annotation.Nullable;
 
 public class BlockModMobCage extends BlockMod {
-	public final ResourceLocation type, spawnItem;
+	public final Identifier type, spawnItem;
 	protected final BlockPos relativePos;
-	public BlockModMobCage(Properties properties, ResourceLocation type, @Nullable ResourceLocation spawnItem, @Nullable BlockPos relativePos) {
-		super(properties.pushReaction(PushReaction.BLOCK));
+
+	public BlockModMobCage(Settings settings, Identifier type, @Nullable Identifier spawnItem, @Nullable BlockPos relativePos) {
+		super(settings.pistonBehavior(PistonBehavior.BLOCK));
 		this.type = type;
 		this.spawnItem = spawnItem;
 		this.relativePos = relativePos;
 	}
-	public BlockModMobCage(ResourceLocation type, ResourceLocation spawnItem) {
-		super(Properties.ofFullCopy(Blocks.SPAWNER).noOcclusion().pushReaction(PushReaction.BLOCK));
+	public BlockModMobCage(Identifier type, Identifier spawnItem) {
+		super(Settings.copy(Blocks.SPAWNER).nonOpaque().pistonBehavior(PistonBehavior.BLOCK));
 		this.type = type;
 		this.spawnItem = spawnItem;
 		relativePos = null;
 	}
-	public BlockModMobCage(ResourceLocation type, ResourceLocation spawnItem, MapColor color) {
-		super(Properties.ofFullCopy(Blocks.SPAWNER).noOcclusion().pushReaction(PushReaction.BLOCK).mapColor(color));
+	public BlockModMobCage(Identifier type, Identifier spawnItem, MapColor color) {
+		super(Settings.copy(Blocks.SPAWNER).nonOpaque().pistonBehavior(PistonBehavior.BLOCK).mapColor(color));
 		this.type = type;
 		this.spawnItem = spawnItem;
 		relativePos = null;
 	}
-	public BlockModMobCage(ResourceLocation type, @Nullable ResourceLocation spawnItem, MapColor color, @Nullable BlockPos relativePos) {
-		super(Properties.ofFullCopy(Blocks.SPAWNER).noOcclusion().pushReaction(PushReaction.BLOCK).mapColor(color));
+	public BlockModMobCage(Identifier type, @Nullable Identifier spawnItem, MapColor color, @Nullable BlockPos relativePos) {
+		super(Settings.copy(Blocks.SPAWNER).nonOpaque().pistonBehavior(PistonBehavior.BLOCK).mapColor(color));
 		this.type = type;
 		this.spawnItem = spawnItem;
 		this.relativePos = relativePos;
 	}
-	@Override public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
-		if(!level.isClientSide && !player.isCreative()) BuiltInRegistries.ENTITY_TYPE.get(type).spawn((ServerLevel) level, null, player, relativePos == null ? pos : pos.offset(relativePos), MobSpawnType.MOB_SUMMONED, true, false);
-		return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+
+	public void trySpawn(World world, BlockPos pos) {
+		if (!world.isClient)
+			Registries.ENTITY_TYPE.get(this.type).spawn(
+					(ServerWorld) world,
+					null,
+					null,  // TODO: Make sure this does not have to be an empty Consumer instead of null
+					this.relativePos == null ? pos : pos.add(this.relativePos),
+					SpawnReason.MOB_SUMMONED,
+					true,
+					false
+			);
 	}
-	@Override public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
-		if(!player.getCooldowns().isOnCooldown(stack.getItem()) && (spawnItem == null || stack.is(BuiltInRegistries.ITEM.get(spawnItem)))) {
-			if(!level.isClientSide) BuiltInRegistries.ENTITY_TYPE.get(type).spawn((ServerLevel) level, null, player, relativePos == null ? pos : pos.offset(relativePos), MobSpawnType.MOB_SUMMONED, true, false);
-			if(spawnItem != null) {
-				player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
-				player.getCooldowns().addCooldown(stack.getItem(), 40);
-				stack.consume(1, player);
-			} return ItemInteractionResult.SUCCESS;
-		} return ItemInteractionResult.FAIL;
+
+	@Override
+	public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+		trySpawn(world, pos);
+		super.onBreak(world, pos, state, player);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+		ItemStack stack = player.getStackInHand(hand);
+		if (stack.isEmpty()) return super.onUse(state, world, pos, player, hand, hit);
+		
+		if(!player.getItemCooldownManager().isCoolingDown(stack.getItem()) && (spawnItem == null || stack.isOf(Registries.ITEM.get(spawnItem)))) {
+			trySpawn(world, pos);
+			if (spawnItem != null) {
+				player.increaseStat(Stats.USED.getOrCreateStat(stack.getItem()), 1);
+				player.getItemCooldownManager().set(stack.getItem(), 40);
+				stack.decrement(1);
+			}
+			return ActionResult.SUCCESS;
+		} return ActionResult.FAIL;
 	}
 }
