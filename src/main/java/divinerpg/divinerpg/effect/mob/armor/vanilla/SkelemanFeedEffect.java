c13993537f8a34ea43db4e2b16191a04e1c62da2
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 12:59:47 +0200

    Ported SkelemanFeedEffect to Fabric

--- src/main/java/divinerpg/divinerpg/effect/mob/armor/vanilla/SkelemanFeedEffect.java
+++ src/main/java/divinerpg/divinerpg/effect/mob/armor/vanilla/SkelemanFeedEffect.java
@@ -1,14 +1,18 @@
-package divinerpg.effect.mob.armor.vanilla;
+package divinerpg.divinerpg.effect.mob.armor.vanilla;
 
-import divinerpg.effect.mob.armor.ArmorEffect;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.LivingEntity;
+import divinerpg.divinerpg.effect.mob.armor.ArmorEffect;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class SkelemanFeedEffect extends ArmorEffect {
-	public SkelemanFeedEffect() {super(10991286);}
-	@Override public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {return true;}
-	@Override public boolean applyEffectTick(LivingEntity entity, int i) {
-		if(entity instanceof ServerPlayer player && player.getFoodData().needsFood()) player.getFoodData().eat(1, 0);
-        return true;
+	public SkelemanFeedEffect() { super(10991286); }
+
+	@Override
+	public boolean canApplyUpdateEffect(int duration, int amplifier) { return true; }
+
+	@Override
+	public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+		if(entity instanceof ServerPlayerEntity player && player.getHungerManager().isNotFull())
+			player.getHungerManager().add(1, 0);
     }
 }
