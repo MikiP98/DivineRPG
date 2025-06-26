7e2347512e6dad3e4b52cfea40aa238b8d7fa27f
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 11:11:55 +0200

    Ported BlockModLog to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModLog.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModLog.java
@@ -1,31 +1,40 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import divinerpg.registries.BlockRegistry;
-import net.minecraft.core.*;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.MapColor;
+import divinerpg.divinerpg.blocks.AlwaysFlammable;
+import divinerpg.divinerpg.registries.BlockRegistry;
+import divinerpg.divinerpg.registries.EntityRegistry;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
-import static divinerpg.registries.EntityRegistry.ENT;
-import static net.minecraft.world.entity.MobSpawnType.MOB_SUMMONED;
-import static net.minecraft.world.level.block.Blocks.OAK_LOG;
+public class BlockModLog extends PillarBlock implements AlwaysFlammable {
+    public BlockModLog(MapColor color, BlockSoundGroup sound) {
+        super(Settings.copy(Blocks.OAK_LOG).mapColor(color).sounds(sound));
+    }
 
-public class BlockModLog extends RotatedPillarBlock {
-    public BlockModLog(MapColor color, SoundType sound) {
-        super(Properties.ofFullCopy(OAK_LOG).mapColor(color).sound(sound));
+    @Override
+    public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack tool) {
+        // TODO: Convert to a set of Vethea logs
+        if (
+                this == BlockRegistry.dreamwoodLog
+                || this == BlockRegistry.firewoodLog
+                || this == BlockRegistry.hyrewoodLog
+                || this == BlockRegistry.mintwoodLog
+        ) {
+            if(!world.isClient) EntityRegistry.ENT.spawn((ServerWorld) world, tool, player, pos, SpawnReason.MOB_SUMMONED, true, false);
+        } super.afterBreak(world, player, pos, state, blockEntity, tool);
     }
-    @Override public void playerDestroy(Level world, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
-        if(this == BlockRegistry.dreamwoodLog.get() || this == BlockRegistry.firewoodLog.get() || this == BlockRegistry.hyrewoodLog.get() || this == BlockRegistry.mintwoodLog.get()) {
-            if(!world.isClientSide) ENT.get().spawn((ServerLevel) world, stack, player, pos, MOB_SUMMONED, true, false);
-        } super.playerDestroy(world, player, pos, state, blockEntity, stack);
-    }
-    @Override public int getFlammability(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {return 5;}
-    @Override public int getFireSpreadSpeed(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {return 5;}
+
+    @Override
+    public int getFlammability() {return 5;}
+    @Override
+    public int getFireSpreadSpeed() {return 5;}
 }
