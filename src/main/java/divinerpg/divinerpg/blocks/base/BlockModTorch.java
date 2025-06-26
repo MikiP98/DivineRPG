68c0330a7daf09ffd7ec1c287cccd1da9d67fee7
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 23:29:12 +0200

    Ported BlockModTorch and BlockModWallTorch to Fabric?

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModTorch.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModTorch.java
@@ -1,24 +1,24 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import divinerpg.registries.*;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockState;
-import net.neoforged.api.distmarker.*;
+import divinerpg.divinerpg.registries.*;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.TorchBlock;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 
 public class BlockModTorch extends TorchBlock {
-    //TODO: I don't know why it refuses to work when I insert the needed particle into the constructor directly
-    public BlockModTorch() {super(ParticleTypes.FLAME, Properties.ofFullCopy(Blocks.TORCH));}
-    @OnlyIn(Dist.CLIENT)
-    @Override public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-        double d0 = pos.getX() + .5;
-        double d1 = pos.getY() + .7;
-        double d2 = pos.getZ() + .5;
-        level.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0, 0, 0);
-        if(this == BlockRegistry.arcaniumTorch.get()) level.addParticle(ParticleRegistry.PURPLE_FLAME.get(), d0, d1, d2, 0, 0, 0);
-        if(this == BlockRegistry.edenTorch.get()) level.addParticle(ParticleRegistry.GREEN_FLAME.get(), d0, d1, d2, 0, 0, 0);
-    }
+//    TODO: I don't know why it refuses to work when I insert the needed particle into the constructor directly
+    // TODO: Check if it works fine on Fabric
+    public BlockModTorch() { super(Settings.copy(Blocks.TORCH), ParticleTypes.FLAME); }
+    public BlockModTorch(ParticleEffect particle) { super(Settings.copy(Blocks.TORCH), particle); }
+
+//    @OnlyIn(Dist.CLIENT)
+//    @Override public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+//        double d0 = pos.getX() + .5;
+//        double d1 = pos.getY() + .7;
+//        double d2 = pos.getZ() + .5;
+//        level.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0, 0, 0);
+//        if(this == BlockRegistry.arcaniumTorch.get()) level.addParticle(ParticleRegistry.PURPLE_FLAME.get(), d0, d1, d2, 0, 0, 0);
+//        if(this == BlockRegistry.edenTorch.get()) level.addParticle(ParticleRegistry.GREEN_FLAME.get(), d0, d1, d2, 0, 0, 0);
+//    }
 }
--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModWallTorch.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModWallTorch.java
@@ -1,26 +1,33 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import divinerpg.registries.*;
-import net.minecraft.core.*;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockState;
-import net.neoforged.api.distmarker.*;
+//import divinerpg.registries.*;
+//import net.minecraft.core.*;
+//import net.minecraft.core.particles.ParticleTypes;
+//import net.minecraft.util.RandomSource;
+//import net.minecraft.world.level.Level;
+//import net.minecraft.world.level.block.*;
+//import net.minecraft.world.level.block.state.BlockState;
+//import net.neoforged.api.distmarker.*;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.block.WallTorchBlock;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 
 public class BlockModWallTorch extends WallTorchBlock {
-    public BlockModWallTorch() {super(ParticleTypes.FLAME, Properties.ofFullCopy(Blocks.WALL_TORCH));}
-    @OnlyIn(Dist.CLIENT)
-    @Override public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
-        double d0 = pos.getX() + .5;
-        double d1 = pos.getY() + .7;
-        double d2 = pos.getZ() + .5;
-        double d3 = .22;
-        double d4 = .27;
-        Direction dir1 = state.getValue(FACING).getOpposite();
-        level.addParticle(ParticleTypes.SMOKE, d0 + d4 * dir1.getStepX(), d1 + d3, d2 + d4 * dir1.getStepZ(), 0, 0, 0);
-        if(this == BlockRegistry.arcaniumWallTorch.get()) level.addParticle(ParticleRegistry.PURPLE_FLAME.get(), d0 + d4 * dir1.getStepX(), d1 + d3, d2 + d4 * dir1.getStepZ(), 0, 0, 0);
-        if(this == BlockRegistry.edenWallTorch.get()) level.addParticle(ParticleRegistry.GREEN_FLAME.get(), d0 + d4 * dir1.getStepX(), d1 + d3, d2 + d4 * dir1.getStepZ(), 0, 0, 0);
-    }
+    public BlockModWallTorch() { super(Settings.copy(Blocks.WALL_TORCH), ParticleTypes.FLAME); }
+    public BlockModWallTorch(ParticleEffect particle) { super(Settings.copy(Blocks.WALL_TORCH), particle); }
+
+//    @OnlyIn(Dist.CLIENT)
+//    @Override public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+//        double d0 = pos.getX() + .5;
+//        double d1 = pos.getY() + .7;
+//        double d2 = pos.getZ() + .5;
+//        double d3 = .22;
+//        double d4 = .27;
+//        Direction dir1 = state.getValue(FACING).getOpposite();
+//        level.addParticle(ParticleTypes.SMOKE, d0 + d4 * dir1.getStepX(), d1 + d3, d2 + d4 * dir1.getStepZ(), 0, 0, 0);
+//        if(this == BlockRegistry.arcaniumWallTorch.get()) level.addParticle(ParticleRegistry.PURPLE_FLAME.get(), d0 + d4 * dir1.getStepX(), d1 + d3, d2 + d4 * dir1.getStepZ(), 0, 0, 0);
+//        if(this == BlockRegistry.edenWallTorch.get()) level.addParticle(ParticleRegistry.GREEN_FLAME.get(), d0 + d4 * dir1.getStepX(), d1 + d3, d2 + d4 * dir1.getStepZ(), 0, 0, 0);
+//    }
 }
