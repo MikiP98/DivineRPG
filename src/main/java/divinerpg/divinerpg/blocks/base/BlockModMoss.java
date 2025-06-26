1833d45fd648fcb9aeb5b2859c312102c90834dd
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-26 18:17:33 +0200

    Ported BlockModMoss to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockModMoss.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockModMoss.java
@@ -1,34 +1,40 @@
-package divinerpg.blocks.base;
+package divinerpg.divinerpg.blocks.base;
 
-import net.minecraft.core.*;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.MossBlock;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.MossBlock;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.WorldView;
 
 public class BlockModMoss extends MossBlock {
-	public BlockModMoss(Properties properties) {
-		super(properties);
+	public BlockModMoss(Settings settings) {
+		super(settings);
 	}
+	
 	@Override
-	public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state) {
-		return level.getBlockState(pos.below()).isAir();
+	public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state, boolean isClient) {
+		return world.getBlockState(pos.down()).isAir();
 	}
+	
 	//TODO: to do it the way vanilla does that
 	@Override
-	public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
-		BlockState myState = this.defaultBlockState();
-		place(level, random, pos.above(), myState);
-		place(level, random, pos.below(), myState);
-		place(level, random, pos.north(), myState);
-		place(level, random, pos.east(), myState);
-		place(level, random, pos.south(), myState);
-		place(level, random, pos.west(), myState);
+	public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+		BlockState myState = this.getDefaultState();
+		place(world, random, pos.up(), myState);
+		place(world, random, pos.down(), myState);
+		place(world, random, pos.north(), myState);
+		place(world, random, pos.east(), myState);
+		place(world, random, pos.south(), myState);
+		place(world, random, pos.west(), myState);
 	}
-	public void place(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
-		if(random.nextBoolean()) pos = pos.relative(Direction.getRandom(random));
-		if(level.getBlockState(pos).is(BlockTags.MOSS_REPLACEABLE)) level.setBlock(pos, state, 3);
+	
+	public void place(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+		if (random.nextBoolean())
+			pos = pos.offset(Direction.random(random));
+		if (world.getBlockState(pos).isIn(BlockTags.MOSS_REPLACEABLE))
+			world.setBlockState(pos, state, 3);
 	}
 }
