4eb7f52c72954592dc35601cedaefef792c9568d
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-27 16:41:27 +0200

    Ported BlockLightCrystal to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/vethea/BlockLightCrystal.java
+++ src/main/java/divinerpg/divinerpg/blocks/vethea/BlockLightCrystal.java
@@ -1,13 +1,12 @@
-package divinerpg.blocks.vethea;
+package divinerpg.divinerpg.blocks.vethea;
 
-import divinerpg.blocks.base.BlockMod;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.*;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.*;
+import divinerpg.divinerpg.blocks.base.BlockMod;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.sound.BlockSoundGroup;
 
 public class BlockLightCrystal extends BlockMod {
     public BlockLightCrystal(MapColor color) {
-        super(BlockBehaviour.Properties.of().mapColor(color).strength(0.3F).sound(SoundType.GLASS).instrument(NoteBlockInstrument.PLING).lightLevel((state) -> 15));
+        super(Settings.create().mapColor(color).strength(0.3F).sounds(BlockSoundGroup.GLASS).instrument(Instrument.PLING).luminance((state) -> 15));
     }
 }
