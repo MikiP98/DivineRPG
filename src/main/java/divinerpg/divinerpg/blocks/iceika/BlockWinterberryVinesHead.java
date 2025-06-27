1efa566443565b3226787eee32446f7b71723976
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-27 14:10:31 +0200

    Ported BlockWinterberryVinesHead to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/iceika/BlockWinterberryVinesHead.java
+++ src/main/java/divinerpg/divinerpg/blocks/iceika/BlockWinterberryVinesHead.java
@@ -1,34 +1,43 @@
-package divinerpg.blocks.iceika;
+package divinerpg.divinerpg.blocks.iceika;
 
-import divinerpg.blocks.arcana.BlockArcaniteVinesHead;
-import divinerpg.registries.*;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.*;
-import net.minecraft.world.level.material.FluidState;
+import divinerpg.divinerpg.blocks.arcana.BlockArcaniteVinesHead;
+import divinerpg.divinerpg.registries.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockWinterberryVinesHead extends BlockArcaniteVinesHead {
-	public static final BooleanProperty RIPE = BlockStateProperties.BLOOM;
+	public static final BooleanProperty RIPE = Properties.BLOOM;
 
-	public BlockWinterberryVinesHead(Properties properties) {
-		super(properties.lightLevel((state) -> 1).dynamicShape());
+	public BlockWinterberryVinesHead(Settings settings) {
+		super(settings.luminance((state) -> 1).dynamicBounds());
 	}
 
-	@Override
-	public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
-		BlockState aboveState = level.getBlockState(pos.above());
-		if (aboveState.getBlock() == BlockRegistry.winterberryVinesBody.get() && aboveState.getValue(RIPE)) {
-			popResource(level, pos.above(), new ItemStack(ItemRegistry.winterberry.get(), 1));
+	// TODO: There is no Fabric equivalent for this method
+//	@Override
+//	public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+//		BlockState aboveState = level.getBlockState(pos.above());
+//		if (aboveState.getBlock() == BlockRegistry.winterberryVinesBody.get() && aboveState.getValue(RIPE)) {
+//			popResource(level, pos.above(), new ItemStack(ItemRegistry.winterberry.get(), 1));
+//		}
+//		return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+//	}
+	// TODO: Using as a replacement for the above method; Make sure this replacement is sufficient
+	@SuppressWarnings("deprecation")
+	public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+		BlockState aboveState = world.getBlockState(pos.up());
+		if (aboveState.getBlock() == BlockRegistry.winterberryVinesBody && aboveState.get(RIPE)) {
+			Block.dropStack(world, pos.up(), new ItemStack(ItemRegistry.winterberry, 1));
 		}
-		return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+		super.onStateReplaced(state, world, pos, newState, moved);
 	}
 
 	@Override
-	protected Block getBodyBlock() {
-		return BlockRegistry.winterberryVinesBody.get();
+	protected Block getPlant() {
+		return BlockRegistry.winterberryVinesBody;
 	}
 }
