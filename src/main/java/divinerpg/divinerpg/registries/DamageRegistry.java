bc7480bae5400d37fd5bdd6d7df7cd47978514df
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-27 21:20:15 +0200

    Ported DamageRegistry to Fabric

--- src/main/java/divinerpg/divinerpg/registries/DamageRegistry.java
+++ src/main/java/divinerpg/divinerpg/registries/DamageRegistry.java
@@ -1,20 +1,25 @@
-package divinerpg.registries;
+package divinerpg.divinerpg.registries;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.world.World;
 
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.damagesource.*;
-import net.neoforged.neoforge.registries.*;
-
-import static divinerpg.DivineRPG.MODID;
+import static divinerpg.divinerpg.DivineRPG.getId;
 
 public class DamageRegistry {
-    public static final DeferredRegister<DamageType> DAMAGE_TYPE = DeferredRegister.create(Registries.DAMAGE_TYPE, MODID);
-    public static final DeferredHolder<DamageType, DamageType>
-        ACID = register("acid", .1F),
-        ARCANA = register("arcana", 0F),
-        SPIKE = register("spike", .1F),
-        TAR = DAMAGE_TYPE.register("tar", () -> new DamageType("tar", .1F, DamageEffects.BURNING)),
-        TURTLE = register("turtle", .1F);
-    private static DeferredHolder<DamageType, DamageType> register(String name, float exhaustion) {
-        return DAMAGE_TYPE.register(name, () -> new DamageType(name, exhaustion));
+    public static final RegistryKey<DamageType>
+        ACID = get("acid"),
+        ARCANA = get("arcana"),
+        SPIKE = get("spike"),
+        TAR = get("tar"),
+        TURTLE = get("turtle");
+
+    private static RegistryKey<DamageType> get(String name) {
+        return RegistryKey.of(RegistryKeys.DAMAGE_TYPE, getId(name));
+    }
+
+    public static DamageSource of(World world, RegistryKey<DamageType> key) {
+        return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
     }
 }
