bdf539627da8b93105816f6e654bdfc78b240d95
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-27 16:13:20 +0200

    Ported BlockArcanaDoubleCrop to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/arcana/BlockArcanaDoubleCrop.java
+++ src/main/java/divinerpg/divinerpg/blocks/arcana/BlockArcanaDoubleCrop.java
@@ -1,23 +1,24 @@
-package divinerpg.blocks.arcana;
+package divinerpg.divinerpg.blocks.arcana;
 
-import divinerpg.blocks.base.BlockModDoubleCrop;
-import divinerpg.util.Utils;
-import net.minecraft.core.*;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.state.BlockState;
+import divinerpg.divinerpg.blocks.base.BlockModDoubleCrop;
+import divinerpg.divinerpg.util.Utils;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
 
-import static divinerpg.registries.BlockRegistry.arcaniteGrass;
+import static divinerpg.divinerpg.registries.BlockRegistry.arcaniteGrass;
 
 public class BlockArcanaDoubleCrop extends BlockModDoubleCrop {
-    public BlockArcanaDoubleCrop(ItemLike seed) {super(seed);}
-    @Override public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
-        for(Direction direction : Direction.Plane.HORIZONTAL) {
-            BlockState blockstate = level.getBlockState(pos.relative(direction));
-            if(blockstate.hasLargeCollisionShape() || level.getFluidState(pos.relative(direction)).is(FluidTags.LAVA)) return false;
-        } BlockState belowState = level.getBlockState(pos.below());
-        return (level.getRawBrightness(pos, 0) >= 8 || level.canSeeSky(pos))
-                && ((belowState.getBlock() == arcaniteGrass.get() && Utils.bordersTar(level, pos.getX(), pos.getY() - 1, pos.getZ()))
-                || belowState.is(this) && belowState.getValue(AGE) == 14);
+    public BlockArcanaDoubleCrop(ItemConvertible seed) {super(seed);}
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockState belowState = world.getBlockState(pos.down());
+        return canPlaceFits(world, pos)
+                && (world.getBaseLightLevel(pos, 0) >= 8 || world.isSkyVisible(pos))
+                && ((belowState.getBlock() == arcaniteGrass && Utils.bordersTar(world, pos.getX(), pos.getY() - 1, pos.getZ()))
+                || belowState.isOf(this)
+                && belowState.get(AGE) == 14);
     }
 }
