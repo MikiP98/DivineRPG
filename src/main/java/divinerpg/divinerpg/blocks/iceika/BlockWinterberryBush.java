aef1d7beceed0803d1852e03967d2294629de031
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-27 12:37:20 +0200

    Ported BlockWinterberryBush to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/iceika/BlockWinterberryBush.java
+++ src/main/java/divinerpg/divinerpg/blocks/iceika/BlockWinterberryBush.java
@@ -1,76 +1,97 @@
-package divinerpg.blocks.iceika;
+package divinerpg.divinerpg.blocks.iceika;
 
-import divinerpg.blocks.base.BlockMod;
-import divinerpg.registries.ItemRegistry;
-import net.minecraft.core.*;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.*;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.*;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.*;
-import net.minecraft.world.level.block.state.properties.*;
-import net.minecraft.world.level.material.*;
-import net.minecraft.world.phys.BlockHitResult;
+import divinerpg.divinerpg.blocks.AlwaysFlammable;
+import divinerpg.divinerpg.blocks.base.BlockMod;
+import divinerpg.divinerpg.registries.ItemRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
-public class BlockWinterberryBush extends BlockMod implements BonemealableBlock {
-	public static final BooleanProperty RIPE = BlockStateProperties.BLOOM;
+public class BlockWinterberryBush extends BlockMod implements Fertilizable, AlwaysFlammable {
+    public static final BooleanProperty RIPE = Properties.BLOOM;
 
     public BlockWinterberryBush() {
-        super((BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).pushReaction(PushReaction.DESTROY).strength(0.2F).randomTicks().noCollission().sound(SoundType.SWEET_BERRY_BUSH).lightLevel((state) -> state.getValue(RIPE) ? 5 : 1)));
-		registerDefaultState(this.stateDefinition.any().setValue(RIPE, false));
+        super(Settings.create()
+                .mapColor(MapColor.DARK_GREEN)
+                .pistonBehavior(PistonBehavior.DESTROY)
+                .strength(0.2F)
+                .ticksRandomly()
+                .noCollision()
+                .sounds(BlockSoundGroup.SWEET_BERRY_BUSH)
+                .luminance((state) -> state.get(RIPE) ? 5 : 1)
+        );
+        setDefaultState(getDefaultState().with(RIPE, false));
     }
 
     @Override
-	public boolean isRandomlyTicking(BlockState state) {
-        return !state.getValue(RIPE);
+    public boolean hasRandomTicks(BlockState state) {
+        return !state.get(RIPE);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (random.nextFloat() < 0.11F) world.setBlockState(pos, state.with(RIPE, true), 3);
     }
 
     @Override
-	public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        if (random.nextFloat() < 0.11F) level.setBlock(pos, state.setValue(RIPE, true), 3);
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        world.setBlockState(pos, state.with(RIPE, true), 3);
     }
 
     @Override
-	public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
-		level.setBlock(pos, state.setValue(RIPE, true), 3);
-	}
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state, boolean isClient){
+        return !state.get(RIPE);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!player.getStackInHand(hand).isEmpty()) return super.onUse(state, world, pos, player, hand, hit);
+
+        if (state.get(RIPE)) {
+            Block.dropStack(world, pos, new ItemStack(ItemRegistry.winterberry, 1));
+            world.playSound(null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
+            world.setBlockState(pos, state.with(RIPE, false), 2);
+            return ActionResult.SUCCESS;
+        } return ActionResult.PASS;
+    }
 
     @Override
-	public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state) {
-		return !state.getValue(RIPE);
-	}
-
-    @Override
-	public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult p_60508_) {
-		if (state.getValue(RIPE)) {
-	        popResource(level, pos, new ItemStack(ItemRegistry.winterberry.get(), 1));
-	        level.playSound(null, pos, SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, SoundSource.BLOCKS, 1.0F, 0.8F + level.random.nextFloat() * 0.4F);
-	        level.setBlock(pos, state.setValue(RIPE, false), 2);
-	        return InteractionResult.SUCCESS;
-		} return InteractionResult.PASS;
-	}
-
-	@Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(RIPE);
     }
 
     @Override
-    public boolean isBonemealSuccess(Level p_180670_1_, RandomSource p_180670_2_, BlockPos p_180670_3_, BlockState p_180670_4_) {
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
         return true;
     }
 
     @Override
-    public int getFlammability(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {
+    public int getFlammability() {
         return 100;
     }
 
     @Override
-    public int getFireSpreadSpeed(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {
+    public int getFireSpreadSpeed() {
         return 60;
     }
 }
