93b673b2af5e5b8ee3420ca04878b95e0b0c0f16
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-27 16:25:20 +0200

    Ported BlockAquaTorch to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/vanilla/BlockAquaTorch.java
+++ src/main/java/divinerpg/divinerpg/blocks/vanilla/BlockAquaTorch.java
@@ -1,44 +1,68 @@
-package divinerpg.blocks.vanilla;
+package divinerpg.divinerpg.blocks.vanilla;
 
-import divinerpg.blocks.base.BlockModTorch;
-import divinerpg.registries.ParticleRegistry;
-import net.minecraft.core.*;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.*;
-import net.minecraft.world.level.block.state.properties.*;
-import net.minecraft.world.level.material.*;
-import net.neoforged.api.distmarker.*;
+import divinerpg.divinerpg.blocks.base.BlockModTorch;
+import divinerpg.divinerpg.registries.ParticleRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 
 import javax.annotation.Nullable;
 
-public class BlockAquaTorch extends BlockModTorch implements SimpleWaterloggedBlock {
-    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    public BlockAquaTorch() {registerDefaultState(stateDefinition.any().setValue(WATERLOGGED, Boolean.FALSE));}
+public class BlockAquaTorch extends BlockModTorch implements Waterloggable {
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+
+    public BlockAquaTorch() {
+        super();
+        setDefaultState(getDefaultState().with(WATERLOGGED, Boolean.FALSE));
+    }
+
     @Nullable
-    @Override public BlockState getStateForPlacement(BlockPlaceContext context) {
-        FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
-        return super.getStateForPlacement(context).setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        FluidState fluidstate = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        BlockState state = super.getPlacementState(ctx);
+        return state == null ? null : state.with(WATERLOGGED, fluidstate.isOf(Fluids.WATER));
     }
-    @Override public BlockState updateShape(BlockState state, Direction dir, BlockState state1, LevelAccessor level, BlockPos pos, BlockPos pos1) {
-        if(state.getValue(WATERLOGGED)) level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
-        return super.updateShape(state, dir, state1, level, pos, pos1);
+
+    @Override
+    public BlockState getStateForNeighborUpdate(
+            BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos
+    ) {
+        if (state.get(WATERLOGGED)) world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
-    @Override protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> state) {
-        state.add(WATERLOGGED);
-        super.createBlockStateDefinition(state);
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(WATERLOGGED);
     }
-    @Override public FluidState getFluidState(BlockState state) {return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : Fluids.EMPTY.defaultFluidState();}
-    @OnlyIn(Dist.CLIENT)
-    @Override public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : Fluids.EMPTY.getDefaultState();
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
         double d0 = pos.getX() + .5;
         double d1 = pos.getY() + .7;
         double d2 = pos.getZ() + .5;
-        if(state.getValue(WATERLOGGED)) level.addParticle(ParticleTypes.BUBBLE, d0, d1, d2, 0, 0, 0);
-        else level.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0, 0, 0);
-        level.addParticle(ParticleRegistry.BLUE_FLAME.get(), d0, d1, d2, 0, 0, 0);
+        if (state.get(WATERLOGGED)) world.addParticle(ParticleTypes.BUBBLE, d0, d1, d2, 0, 0, 0);
+        else world.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0, 0, 0);
+        world.addParticle(ParticleRegistry.BLUE_FLAME, d0, d1, d2, 0, 0, 0);
     }
 }
