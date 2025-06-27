5fd3a6fd0266b222354260d3f5abff7dd8cbc432
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-27 12:26:22 +0200

    BlockMod update - removed unnecessary code

--- src/main/java/divinerpg/divinerpg/blocks/base/BlockMod.java
+++ src/main/java/divinerpg/divinerpg/blocks/base/BlockMod.java
@@ -9,19 +9,23 @@ import net.minecraft.sound.BlockSoundGroup;
 
 public class BlockMod extends Block {
     //Blocks with specific properties
-    public BlockMod(Block.Settings settings) {super(settings);}
+    public BlockMod(Settings settings) {super(settings);}
     //Regular stone-like properties
     public BlockMod(MapColor color) {this(color, 1.5F, 6);}
     //Regular stone-like properties, but custom hardness/resistance
     public BlockMod(MapColor color, float hardness, float resistance) {
-        super(Block.Settings.copy(Blocks.STONE).mapColor(color).strength(hardness, resistance));
+        super(Settings.copy(Blocks.STONE).mapColor(color).strength(hardness, resistance));
     }
     //Custom hardness/resistance + instrument and sound
     public BlockMod(MapColor color, float hardness, float resistance, BlockSoundGroup sound, Instrument instrument) {
-        super(Block.Settings.create().mapColor(color).requiresTool().strength(hardness, resistance).sounds(sound).instrument(instrument));
+        super(Settings.create().mapColor(color).requiresTool().strength(hardness, resistance).sounds(sound).instrument(instrument));
+    }
+    // For BlockModLamp
+    public BlockMod(MapColor color, float hardness, float resistance, BlockSoundGroup sound, Instrument instrument, int luminance) {
+        super(Settings.create().mapColor(color).requiresTool().strength(hardness, resistance).sounds(sound).instrument(instrument).luminance((state) -> luminance));
     }
     //Hard blocks like obsidian, but luminous
     public BlockMod(MapColor color, int luminance) {
-        super(Block.Settings.copy(Blocks.OBSIDIAN).mapColor(color).pistonBehavior(PistonBehavior.BLOCK).luminance((state) -> luminance));
+        super(Settings.copy(Blocks.OBSIDIAN).mapColor(color).pistonBehavior(PistonBehavior.BLOCK).luminance((state) -> luminance));
     }
 }
