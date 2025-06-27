738c36cba88a893c5e002b14d6f5e2f4191f472e
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-27 10:45:27 +0200

    Ported BlockCandyCane to Fabric

--- src/main/java/divinerpg/divinerpg/blocks/iceika/BlockCandyCane.java
+++ src/main/java/divinerpg/divinerpg/blocks/iceika/BlockCandyCane.java
@@ -1,18 +1,27 @@
-package divinerpg.blocks.iceika;
+package divinerpg.divinerpg.blocks.iceika;
 
-import com.mojang.serialization.MapCodec;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.*;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
 
-public class BlockCandyCane extends HorizontalDirectionalBlock {
-	public static final MapCodec<BlockCandyCane> CODEC = simpleCodec(BlockCandyCane::new);
-	@Override public MapCodec<BlockCandyCane> codec() {return CODEC;}
-	public BlockCandyCane(Properties properties) {
-		super(properties.instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(.75F).pushReaction(PushReaction.PUSH_ONLY));
+import org.jetbrains.annotations.NotNull;
+
+public class BlockCandyCane extends HorizontalFacingBlock {
+	public BlockCandyCane(Settings settings) {
+		super(settings.instrument(Instrument.BASEDRUM).requiresTool().strength(.75F).pistonBehavior(PistonBehavior.PUSH_ONLY));
 	}
-	@Override protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_53681_) {p_53681_.add(FACING);}
-	@Override public BlockState getStateForPlacement(BlockPlaceContext p_53679_) {return this.defaultBlockState().setValue(FACING, p_53679_.getHorizontalDirection().getOpposite());}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+
+	@Override
+	public @NotNull BlockState getPlacementState(ItemPlacementContext ctx) {
+		return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+	}
 }
