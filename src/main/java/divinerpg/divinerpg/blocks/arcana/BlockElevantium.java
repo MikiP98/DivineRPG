0a5ff82afd61745f281911b8f9dbdfef8557643e
MikiP98 <pokora.mikolaj@gmail.com>
2025-06-28 17:59:01 +0200

    Ported BlockElevantium to Fabric (without redstone redirection)

--- src/main/java/divinerpg/divinerpg/blocks/arcana/BlockElevantium.java
+++ src/main/java/divinerpg/divinerpg/blocks/arcana/BlockElevantium.java
@@ -1,41 +1,55 @@
-package divinerpg.blocks.arcana;
+package divinerpg.divinerpg.blocks.arcana;
 
-import divinerpg.blocks.base.BlockMod;
-import divinerpg.registries.SoundRegistry;
-import net.minecraft.core.*;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.*;
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.shapes.*;
-
-import javax.annotation.Nullable;
+import divinerpg.divinerpg.blocks.base.BlockMod;
+import divinerpg.divinerpg.registries.SoundRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.entity.Entity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class BlockElevantium extends BlockMod {
-    protected static final VoxelShape ELEVANTIUM = Shapes.or(box(1, 0, 1, 15, 1, 15),
-                                                             box(4.5, 1, 4.5, 11.5, 2, 11.5));
+    protected static final VoxelShape ELEVANTIUM = VoxelShapes.union(
+            createCuboidShape(1, 0, 1, 15, 1, 15),
+            createCuboidShape(4.5, 1, 4.5, 11.5, 2, 11.5)
+    );
+
     public BlockElevantium() {
-        super(Properties.ofFullCopy(Blocks.DIAMOND_BLOCK).mapColor(MapColor.COLOR_LIGHT_BLUE).pushReaction(PushReaction.DESTROY));
+        super(Settings.copy(Blocks.DIAMOND_BLOCK).mapColor(MapColor.LIGHT_BLUE).pistonBehavior(PistonBehavior.DESTROY));
     }
-    @Override public void stepOn(Level world, BlockPos pos, BlockState state, Entity entity) {
-        int signal = world.getBestNeighborSignal(pos);
-        if(signal > 0) {
-            entity.addDeltaMovement(new Vec3(0, signal * .25, 0));
-            entity.hasImpulse = true;
-            world.playSound(null, pos, SoundRegistry.CONSTRUCTOR_PUNCH.get(), SoundSource.BLOCKS, .75F, .9F);
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        int signal = world.getReceivedRedstonePower(pos);
+        if (signal > 0) {
+            entity.addVelocity(0, signal * .25, 0);
+            world.playSound(null, pos, SoundRegistry.CONSTRUCTOR_PUNCH, SoundCategory.BLOCKS, .75F, .9F);
         }
     }
-    @Override public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         return ELEVANTIUM;
     }
-    @Override public VoxelShape getCollisionShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
-        return Shapes.create(ELEVANTIUM.bounds().inflate(.0625, .125, .0625));
-    }
+
+    @SuppressWarnings("deprecation")
     @Override
-    public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction) {
-        return true;
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(ELEVANTIUM.getBoundingBox().expand(.0625, .125, .0625));
     }
+
+    // TODO: There is no Fabric equivalent for this method.
+    //  Find an alternative or ignore it.
+//    @Override
+//    public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction) {
+//        return true;
+//    }
 }
